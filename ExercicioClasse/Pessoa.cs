d8fa7be290276b0b64e57a9f57a05e9a7e959154
Eduardo Gomes <eduardogomes7@live.com>
2025-04-23 11:27:52 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioClasse/Pessoa.cs
@@ -0,0 +1,8 @@
+﻿namespace ExercicioClasse
+{
+    internal class Pessoa
+    {
+        public string Nome;
+        public int Idade;
+    }
+}
---
+++ ExercicioClasse/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Runtime.ConstrainedExecution;
+
+
+namespace ExercicioClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // EXERCÍCIO 01
+            // Fazer um programa para ler os dados de duas pessoas, depois mostrar o nome da pessoa mais velha.
+
+            Pessoa pessoa1, pessoa2;
+
+            pessoa1 = new Pessoa();
+            pessoa2 = new Pessoa();
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            pessoa1.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoa1.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            pessoa2.Nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoa2.Idade = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            if(pessoa1.Idade > pessoa2.Idade)
+            {
+                Console.WriteLine("Pessoa mais velha: " + pessoa1.Nome);
+            }
+            else
+            {
+                Console.WriteLine("Pessoa mais velha: " + pessoa2.Nome);
+            }
+
+            // EXERCÍCIO 02
+            // Fazer um programa para ler nome e salário de dois funcionários.Depois, mostrar o salário 
+            // médio dos funcionários. 
+
+            Funcionario func1, func2;
+
+            func1 = new Funcionario();
+            func2 = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário:");
+            Console.Write("Nome: ");
+            func1.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            func1.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            Console.WriteLine("Dados do segundo funcionário:");
+            Console.Write("Nome: ");
+            func2.Nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            func2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            double salarioMedio = (func1.Salario + func2.Salario) / 2.0;
+
+            Console.WriteLine("Salário médio = " + salarioMedio.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+        }
+    }
+}
